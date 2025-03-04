f9b01f502a8ec990e0b1e7fce8a3c10900c2d8cc
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-04 11:53:40 -0500

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// Testing functionality
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
