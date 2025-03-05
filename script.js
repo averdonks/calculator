5086fe3f25a23386afa2152434fbabfa4d1f93e7
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-04 19:29:50 -0500

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const nums = document.querySelectorAll(".num");
+const opers = document.querySelectorAll(".oper");
 const display = document.querySelector("#display");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
 
 let firstNum = 0;
 let secondNum = 0;
 let operator = 0;
+let displayText = display.textContent;
+let toggleNum = false;
+let onFirst = true;
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -36,25 +43,52 @@ function operate(oper, num1, num2) {
 
 nums.forEach((num) => {
     num.addEventListener("click", () => {
-        console.log("clicked");
-        display.textContent = display.textContent.concat(num.textContent);
+        if (displayText === "0" || toggleNum) {
+            displayText = num.textContent;
+            toggleNum = false;
+        } else {
+            displayText = displayText.concat(num.textContent);
+        }
+        
+        display.textContent = displayText;
+
+        if (onFirst) {
+            firstNum = +displayText;
+        } else {
+            secondNum = +displayText;
+        }
+
+        toggleNum = false;
+        console.log(display.textContent);
+        console.log(displayText);
+
     });
 });
 
-// Testing functionality
+opers.forEach((oper) => {
+    oper.addEventListener("click", () => {
+        operator = oper.textContent;
+        toggleNum = true;
+        if (onFirst) {
+            onFirst = false;
+        } else {
+            onFirst = true;
+        }
+    });
+});
 
-firstNum = 10;
-secondNum = 5;
+equal.addEventListener("click", () => {
+    display.textContent = operate(operator, firstNum, secondNum);
+    console.log(operator);
+    console.log(firstNum);
+    console.log(secondNum);
+    onFirst = true;
+})
 
-operator = "+";
-console.log(operate(operator, firstNum, secondNum));
-
-operator = "-";
-console.log(operate(operator, firstNum, secondNum));
-
-operator = "*";
-console.log(operate(operator, firstNum, secondNum));
-
-operator = "/";
-console.log(operate(operator, firstNum, secondNum));
+clear.addEventListener("click", () => {
+    display.textContent = "0";
+    displayText = "0";
+    firstNum = 0;
+    secondNum = 0;
+})
 
