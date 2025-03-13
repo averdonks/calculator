f4c8cec89ae7a2f5e2b3b1f8ff928e9fb99b2fe2
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-13 10:48:15 -0400

    Rework code to meet specifications

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const display = document.querySelector("#display");
 const equal = document.querySelector("#equal");
 const clear = document.querySelector("#clear");
 
-let firstNum = 0;
-let secondNum = 0;
-let operator = 0;
+let firstNum = "0";
+let secondNum = "0";
+let operator = "";
+// store display contents in a variable
 let displayText = display.textContent;
-let toggleNum = false;
-let onFirst = true;
-
+// save previous button press in "state" variable
+let state = "";
+let operCount = 0;
+let hasOperated = false;
+let secondToggle = false;
+// let refresh = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -41,54 +45,122 @@ function operate(oper, num1, num2) {
     }
 }
 
+function reset() {
+    displayText = "0";
+    display.textContent = displayText;
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
+    operCount = 0;
+    hasOperated = false;
+    state = "";
+}
+
 nums.forEach((num) => {
     num.addEventListener("click", () => {
-        if (displayText === "0" || toggleNum) {
+        // reset display after pressing a new number after calculation
+        if (state === "equal") {
+            reset();
             displayText = num.textContent;
-            toggleNum = false;
+            display.textContent = displayText;
+            firstNum = displayText;
+            // refresh = true;
+            return;
+        }
+
+        // replace displayText on 0, operation change, or division by 0
+        if ((displayText === "0" || state === "oper") || (displayText === "I can't let you do that.")) {
+            displayText = num.textContent;
+            // if operator changed, set secondNum
+            if (state === "oper") {
+                secondNum = num.textContent;
+                secondToggle = true;
+            }
+            // otherwise, concat num
         } else {
             displayText = displayText.concat(num.textContent);
+            if (secondToggle) {
+                secondNum = displayText;
+            }
         }
-        
         display.textContent = displayText;
-
-        if (onFirst) {
-            firstNum = +displayText;
-        } else {
-            secondNum = +displayText;
-        }
-
-        toggleNum = false;
-        console.log(display.textContent);
-        console.log(displayText);
-
+        state = "num";
     });
 });
 
 opers.forEach((oper) => {
     oper.addEventListener("click", () => {
-        operator = oper.textContent;
-        toggleNum = true;
-        if (onFirst) {
-            onFirst = false;
-        } else {
-            onFirst = true;
+        
+        if (state !== "oper" && state !== "equal") {
+            operCount++;
         }
+
+        // perform operation if two numbers have been inputed
+        if ((operCount % 2 === 0 || hasOperated) && (state !== "oper" && state !== "equal")) {
+            console.log("one")
+            // stop division by 0
+            if (firstNum >= 0 && secondNum == "0") {
+                reset();
+                console.log("don't")
+                displayText = "I can't let you do that.";
+                display.textContent = displayText;
+                return;
+            }
+            let result = operate(operator, +firstNum, +secondNum);
+            // convert numbers larger than or equal to one million to exponential notation
+            if (result >= 1000000) {
+                result = result.toExponential(2);
+            }
+            firstNum = result;
+            console.log(firstNum);
+            console.log(secondNum);
+            console.log("operating");
+            displayText = firstNum;
+            display.textContent = displayText;
+            hasOperated = true;
+            secondToggle = false;
+        }
+
+        if (state === "num") {
+            firstNum = displayText;
+        }
+
+        operator = oper.textContent;
+        state = "oper";
     });
 });
 
 equal.addEventListener("click", () => {
-    display.textContent = operate(operator, firstNum, secondNum);
-    console.log(operator);
-    console.log(firstNum);
-    console.log(secondNum);
-    onFirst = true;
+    // stop division by 0
+    if (firstNum >= 0 && secondNum === "0" && operator == "/") {
+        reset();
+        displayText = "I can't let you do that."
+        display.textContent = displayText;
+        return;
+    } 
+    
+    // stop display from disappearing when pressing a num followed by =
+    if (secondNum == "0" && state != "oper") {
+        return;
+    }
+
+    if (state !== "equal") {
+        // operate by self if operator and equals pressed in sequence
+        if (state === "oper") {
+            secondNum = firstNum;
+        }
+        let result = operate(operator, +firstNum, +secondNum);
+        if (result >= 1000000) {
+            result = result.toExponential(2);
+        }
+        firstNum = result;
+        displayText = firstNum;
+        display.textContent = displayText;
+        hasOperated = true;
+        secondToggle = false;
+        state = "equal";
+    }
+ 
 })
 
-clear.addEventListener("click", () => {
-    display.textContent = "0";
-    displayText = "0";
-    firstNum = 0;
-    secondNum = 0;
-})
-
+clear.addEventListener("click", reset); 
